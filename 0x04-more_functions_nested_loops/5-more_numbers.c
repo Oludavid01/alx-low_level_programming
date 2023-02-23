d4999421a7d1270be619ca5c6512673ac291656b
Oludavid01 <foludavid01@gmail.com>
2023-02-23 10:05:03 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_more_numbers - Prints 10 times the nmber since 0 up to 14
+ * Return: 10 times the number since 0 up to 14
+ */
+void print_more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 0)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
