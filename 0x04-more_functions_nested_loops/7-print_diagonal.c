658e66d18b5964b9ac256dde0224e9fa62ba1e1f
Oludavid01 <foludavid01@gmail.com>
2023-02-23 13:03:48 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,12 +18,12 @@ void print_diagonal(int n)
 	{
 		for (x = 0; x < n; x++)
 		{
-			for (y = 0; y < n; y++)
+			for (y = 0; y < x; y++)
 			{
 				_putchar(32);
 			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
