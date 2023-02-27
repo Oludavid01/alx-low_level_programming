5c5a25bf6d5ba9fb1daa645929ddf3ef8e298616
Oludavid01 <foludavid01@gmail.com>
2023-02-27 13:40:14 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char password(84);
+	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
@@ -26,7 +26,7 @@ int main(void)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
-		
+
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
 		for (index = 0; password[index]; index++)
@@ -39,7 +39,7 @@ int main(void)
 		}
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_hlf2))
+			if (password[index] >= (33 + diff_half2))
 			{
 				password[index] -= diff_half2;
 				break;
